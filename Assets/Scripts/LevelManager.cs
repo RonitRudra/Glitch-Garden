2afd64b34b8a29d9c419b04adb2247caf22ed971
Ronit Rudra <ronit.rudra1995@gmail.com>
2017-05-31 20:13:34 -0500

    Splash and Start

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	float loadDelay = 3f;
+	
+	void Start(){
+		Invoke("LoadNextLevel",loadDelay);
+	}
+	
+	public void LoadLevel(string name){
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+	
+	
+}
